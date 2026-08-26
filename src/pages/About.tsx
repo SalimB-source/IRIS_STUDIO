736bea8daf6a5b6e3381be10792961f00aaef0c9
SalimB-source <2.84957636e+08+SalimB-source@users.noreply.github.com>
2026-08-26 22:18:40 +0000

    Mettre à jour le logo Danone

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -113,7 +113,7 @@ export const brandPartners = [
   { name: "Ooredoo", logo: "assets/ooredoo-logo-provided_5e9d03a0.png" },
   { name: "Philip Morris International", logo: "assets/philip-morris-logo_9da5a02d.png" },
   { name: "Bel", logo: "assets/bel-logo-officiel-2019.webp" },
-  { name: "Danone", logo: "assets/danone-logo_ad3b4003.png" },
+  { name: "Danone", logo: "assets/danone-logo-fourni.png" },
   { name: "Algérie Télécom", logo: "assets/algerie-telecom-logo_58b44931.png" },
   { name: "Djezzy", logo: "assets/djezzy-logo_827e6b0c.svg" },
   { name: "Lafarge", logo: "assets/lafarge-logo_cd94104e.png" },
