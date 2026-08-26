9bfd83d0201166b63b5f0a45ce2d241cf8c2b0b8
SalimB-source <2.84957636e+08+SalimB-source@users.noreply.github.com>
2026-08-26 22:12:58 +0000

    Mettre à jour le logo Bel officiel

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -112,7 +112,7 @@ export const brandPartners = [
   { name: "Henkel", logo: "assets/henkel-logo-transparent-provided_47000655.png" },
   { name: "Ooredoo", logo: "assets/ooredoo-logo-provided_5e9d03a0.png" },
   { name: "Philip Morris International", logo: "assets/philip-morris-logo_9da5a02d.png" },
-  { name: "Bel", logo: "assets/bel-logo_b884da95.jpg" },
+  { name: "Bel", logo: "assets/bel-logo-officiel-2019.webp" },
   { name: "Danone", logo: "assets/danone-logo_ad3b4003.png" },
   { name: "Algérie Télécom", logo: "assets/algerie-telecom-logo_58b44931.png" },
   { name: "Djezzy", logo: "assets/djezzy-logo_827e6b0c.svg" },
